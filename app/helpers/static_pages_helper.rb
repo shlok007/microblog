f26d8013bfded05a0b1af0d88fbc169ac12e44e1
rahul <rahulsingh2240@gmail.com>
2017-06-20 11:47:59 +0530

    Static-pages added

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,11 @@
 module StaticPagesHelper
+  def full_title(title='')
+      base_title='Pithugram'
+      if title.empty?
+        base_title
+      else
+        title+'|'+base_title
+      end
+
+  end
 end
